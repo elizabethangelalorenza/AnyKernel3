bb37ad63ca3106a3f8488538073b6910c716bd3e
Nauval Rizky <enuma.alrizky@gmail.com>
2023-02-26 06:30:43 +0700

    patch: Bring back build.prop detection
    Change-Id: I16e04484c6fd496d1e3dd10a1cd60422cee4c582

--- tools/kyriepatch.sh
+++ tools/kyriepatch.sh
@@ -1,10 +1,12 @@
 # cleanup it first
 patch_cmdline msm_dsi.phyd_multiplier=10 ""
 
-# begin patching
-if contains $ZIPFILE "miui.zip"; then
-    ui_print "Setting up panel compatibility for MIUI...";
+build_ver=$(file_getprop /system/build.prop ro.system.build.version.incremental)
+if contains "$ZIPFILE" "miui.zip" \
+    || contains "$build_ver" "V13." \
+    || contains "$build_ver" "V14." ; then
+    ui_print "MIUI is detected: $build_ver";
+    ui_print "Setting up panel compatibility...";
     patch_cmdline androidboot.hardware=qcom \
         "androidboot.hardware=qcom msm_dsi.phyd_multiplier=10"
-    ui_print "Done.";
 fi
